0dc7acaf5470ef0003cffafadaae88ce852f23a1
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-03-12 13:46:04 -0500

    Swap button goes from Red to Yellow

--- client.js
+++ client.js
@@ -6,11 +6,17 @@ $(document).ready(readyNow);
 function readyNow(){
   console.log('jQuery');
   $('#generate').on('click', addNewDiv);
+  $('#container').on('click', '.swap', changeColor);
 }
 
 function addNewDiv(){
   numberOfClicks ++;
-  let newButtons = ('<button id ="swap">Swap</button> <button id="delete">Delete</button>');
-  $('#container').append('<div class="clicks"><p>' + numberOfClicks + '</p>' + newButtons + '</div>');
+  let newButtons = ('<button class="swap">Swap</button>' + '<button class="delete">Delete</button>');
+  $('#container').append('<div class="buttonclicks"><p>' + numberOfClicks + '</p>' + newButtons + '</div>');
   console.log(newButtons);
+}
+
+function changeColor(){
+  console.log('swapping colors');
+  $(this).parent().toggleClass('highlight');
 }
