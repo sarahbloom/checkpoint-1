27ed2e9cdf782f042f7a4efeb6eb9abb09881da3
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-03-12 13:49:49 -0500

    delete botton click removes div

--- client.js
+++ client.js
@@ -7,16 +7,22 @@ function readyNow(){
   console.log('jQuery');
   $('#generate').on('click', addNewDiv);
   $('#container').on('click', '.swap', changeColor);
+  $('#container').on('click', '.delete', removeDiv);
 }
 
 function addNewDiv(){
   numberOfClicks ++;
+  console.log("button click : ", numberOfClicks);
   let newButtons = ('<button class="swap">Swap</button>' + '<button class="delete">Delete</button>');
   $('#container').append('<div class="buttonclicks"><p>' + numberOfClicks + '</p>' + newButtons + '</div>');
-  console.log(newButtons);
 }
 
 function changeColor(){
   console.log('swapping colors');
   $(this).parent().toggleClass('highlight');
+}
+
+function removeDiv(){
+  console.log("removing div");
+  $(this).parent().remove();
 }
