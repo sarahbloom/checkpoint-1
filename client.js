116f81138ad79e4e2fbd4e6808c944f5cdee54c4
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-03-12 14:01:54 -0500

    changed code to append swap and delete buttons

--- client.js
+++ client.js
@@ -13,12 +13,13 @@ function  addClickHandlers(){
   $('#container').on('click', '.swap', changeColor);
   $('#container').on('click', '.delete', removeDiv);
 }
-  
+
 function addNewDiv(){
   numberOfClicks ++;
   console.log("button click : #", numberOfClicks);
   let newButtons = ('<button class="swap">Swap</button>' + '<button class="delete">Delete</button>');
-  $('#container').append('<div class="buttonclicks"><p>' + numberOfClicks + '</p>' + newButtons + '</div>');
+  $('#container').append('<div class="buttonclicks"><p>' + numberOfClicks + '</p></div>');
+  $('.buttonclicks').last().append(newButtons);
 }
 
 function changeColor(){
